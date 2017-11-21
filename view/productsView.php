ab05a90e4be5a826b43967108dd661281cd64fa1
Quentin Martinez <quentin.martinez@outlook.com>
2017-11-21 17:28:04 +0100

    factory & MVC

--- view/productsView.php
+++ view/productsView.php
@@ -9,10 +9,20 @@
                 <div class="card-content" style="height: 60px">
                     <p style="font-size: 16px"> <?= $product->Desc ?></p>
                 </div>   
-                <div class="card-action">
-                <p>Prix: <?= $product->Price ?> €</p>
-                    <a href="#" id="<?=$product->Id ?>" class="btn btn-sm btn-success">Ajouter au panier</a>
-                </div>
+                <center>
+				<div class="card-action">
+					<p>Prix: <?= $product->Price ?> €</p>
+						<a href="#" id="<?=$product->Id ?>" class="btn btn-sm btn-success" onclick="showProduct(<?=$product->Id ?>)">Voir l'article</a>
+					</div>
+				</center>
             </div>
         </div>
 <?php endforeach; ?>
+<script type="text/javascript">
+                <!--
+                        function showProduct($id)
+                        {
+                              window.open('View/viewProduct.php','nom_de_ma_popup','menubar=no, scrollbars=no, top=100, left=100, width=300, height=200');
+                        }
+                -->
+</script>
