f3985766572902b1b0c6cd542d13aa6907b9b104
Quentin Martinez <quentin.martinez@outlook.com>
2017-12-20 14:52:50 +0100

    bdd

--- model/product.php
+++ model/product.php
@@ -5,9 +5,8 @@ class Product{
 	public $Name;
 	public $Price;
 	public $Reference;
-    public function __construct($_id, $_label, $_name, $_price, $_reference) {
+    public function __construct($_id, $_name, $_price, $_reference) {
 		$this->Id = $_id;
-		$this->Label = $_label;
 		$this->Name = $_name;
 		$this->Price = $_price;
 		$this->Reference = $_reference;
