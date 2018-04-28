5d94f1152993f7de04186438baf04b1c5ab36c64
Quentin Martinez <quentinmartinez@MacBook-Pro-de-Quentin.local>
2018-04-28 17:23:31 +0200

    tCustomers

--- manager/customersManager.php
+++ manager/customersManager.php
@@ -5,7 +5,7 @@ require_once('model/customer.php');
 class CustomersManager extends Model {
 	// Renvoie la un Customer sur le critére correspondant. Id/Username suivant la provenance de l'appel. Login/OrderInfo (Ex: qmz , 1)
 	public function get($identifier){
-		$req = 'SELECT * FROM tcustomers WHERE ';
+		$req = 'SELECT * FROM tCustomers WHERE ';
 		if (is_string($identifier))	{
 			$req = $req.'username=?';
 		}
@@ -22,7 +22,7 @@ class CustomersManager extends Model {
 	}
 
 	public function update($Customer){
-		$req = 'UPDATE tcustomers SET username = ?, firstname = ?, lastname = ?, password = ?, email = ?, enabled = ?, rights = ? WHERE username=?';
+		$req = 'UPDATE tCustomers SET username = ?, firstname = ?, lastname = ?, password = ?, email = ?, enabled = ?, rights = ? WHERE username=?';
 		$client = $this->executerRequete($req, array(
 			$Customer->username(),
 			$Customer->firstName(), 
